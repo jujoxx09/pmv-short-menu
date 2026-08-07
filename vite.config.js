4cadc34b741fe6c3ca5fcc83d0f38f731797a0ad
jujoxx09 <juanjunior.sa@gmail.com>
2026-08-07 17:52:38 +0200

    cambio en vite config para que funcione en vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+
+// esto es para que funcione con github pages.
+/*export default defineConfig({
   plugins: [react()],
   base: '/pmv-short-menu/',
+})*/
+
+export default defineConfig({
+  plugins: [react()],
 })
